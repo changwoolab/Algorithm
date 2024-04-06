ddd72e2822a7c455c41bd23b3b29cceb93da3adb
Changwoo Yoo <cwyoo01@naver.com>
2024-04-07 02:15:36 +0900

    Create 25314.cpp

---
+++ Baekjoon/25314.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <queue>
+#include <set>
+#include <unordered_map>
+#include <map>
+#include <list>
+#include <stack>
+#define pi pair<int, int>
+#define NOT_DEFINED 987654321
+
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  std::cout.tie(NULL);
+
+  int n;
+  cin >> n;
+
+  for (int i = 0; i < n/4; i++) {
+    cout << "long ";
+  }
+
+  cout << "int";
+}
